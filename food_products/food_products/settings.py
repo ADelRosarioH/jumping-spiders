2b62ed0ef4f1b9a7fdf6ed61b39a511bdb05ccd5
Anthony <adelrosarioh@gmail.com>
2020-05-24 15:51:56 -0400

    fix: custom settings per spider
    the idea is to have multiple spiders per project (category)
    this approach let me have the config per spider. avoid globla conflicts
    later.

--- food_products/food_products/settings.py
+++ food_products/food_products/settings.py
@@ -64,11 +64,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-    'food_products.pipelines.BasicBasketsPdfsToS3Pipeline': 300,
-}
+# ITEM_PIPELINES = {
+#    'food_products.pipelines.BasicBasketsPipeline': 300
+# }
 
-FILES_STORE = 's3://jumping-spiders/food_products/basic_baskets/'
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
--- food_products/food_products/spiders/basic_baskets.py
+++ food_products/food_products/spiders/basic_baskets.py
@@ -2,23 +2,31 @@
 import os
 import scrapy
 from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import Spider
 from food_products.items import BasicBasketsItem
 
 
-class BasicBasketsSpider(CrawlSpider):
+class BasicBasketsSpider(Spider):
     name = 'basic_baskets'
-    allowed_domains = ['']
-    start_urls = [
-        'https://proconsumidor.gob.do/sondeos-de-canasta-basica-2020/',
-    ]
 
-    rules = (
-        Rule(LinkExtractor(), callback='parse', follow=True),
-    )
+    custom_settings = {
+        'FILES_STORE': 's3://jumping-spiders/food_products/basic_baskets/',
+        'ITEM_PIPELINES': {
+            'food_products.pipelines.BasicBasketsAvoidPdfsDuplicatesPipeline': 100,
+            'food_products.pipelines.BasicBasketsPdfsToS3Pipeline': 200,
+            'food_products.pipelines.BasicBasketsPdfsIndexingPipeline': 300,
+        },
+    }
+
+    def __init__(self, connection_string=None, *args, **kwargs):
+        super(BasicBasketsSpider, self).__init__(*args, **kwargs)
+        self.connection_string = connection_string
+
+    def start_requests(self):
+        yield scrapy.Request(self.start_url)
 
     def parse(self, response):
         item = BasicBasketsItem()
-        item["file_urls"] = response.css(
+        item['file_urls'] = response.css(
             'div.filetitle a::attr(href)').getall()
         return item
