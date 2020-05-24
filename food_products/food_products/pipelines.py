060a0db57a3642c7c9316be0016209e23475d6de
Anthony <adelrosarioh@gmail.com>
2020-05-24 15:50:29 -0400

    add: avoid duplicates and indexing pipeline
    Avoid downloading/uploading files that were indexed.
    if new files are found then upload to s3
    and then index them

--- food_products/food_products/pipelines.py
+++ food_products/food_products/pipelines.py
@@ -12,6 +12,23 @@ from scrapy.pipelines.files import FilesPipeline
 from scrapy.exceptions import DropItem
 from scrapy.utils.python import to_bytes
 from urllib.parse import urlparse
+from food_products.utils.database import get_session
+
+
+class BasicBasketsAvoidPdfsDuplicatesPipeline:
+
+    def process_item(self, item, spider):
+        connection_string = spider.connection_string
+        session = get_session(connection_string)
+
+        for file_url in item['file_urls']:
+            file_hash = hashlib.sha1(to_bytes(file_url)).hexdigest()
+            result = session.execute(
+                "SELECT 1 FROM basic_baskets_indexing WHERE file_hash = '{}' LIMIT 1".format(file_hash))
+            if result:
+                item['file_urls'].remove(file_url)
+
+        return item
 
 
 class BasicBasketsPdfsToS3Pipeline(FilesPipeline):
@@ -33,3 +50,30 @@ class BasicBasketsPdfsToS3Pipeline(FilesPipeline):
     def get_media_requests(self, item, info):
         for file_url in item['file_urls']:
             yield scrapy.Request(file_url)
+
+    def item_completed(self, results, item, info):
+        file_paths = [x['path'] for ok, x in results if ok]
+        if not file_paths:
+            raise DropItem("Item contains no files")
+        item['file_paths'] = file_paths
+        return item
+
+
+class BasicBasketsPdfsIndexingPipeline:
+    def process_item(self, item, spider):
+        connection_string = spider.connection_string
+        session = get_session(connection_string)
+
+        for (file_url, file_path) in zip(item['file_urls'], item['file_paths']):
+            file_hash = hashlib.sha1(to_bytes(file_url)).hexdigest()
+            file = {
+                'file_hash': file_hash,
+                'file_url': file_url,
+                'file_path': file_path
+            }
+            session.execute("""INSERT INTO basic_baskets_indexing (file_hash, file_url, file_path) 
+                VALUES (:file_hash, :file_url, :file_path)""", file)
+
+        session.commit()
+
+        return item
