74fd99248c19186000706ccffe0e03f8e79f801c
Anthony <adelrosarioh@gmail.com>
2020-07-09 13:53:10 -0400

    removes custom config that will no longer be used

--- jumping-spiders/jumping_spiders/spiders/basic_baskets.py
+++ jumping-spiders/jumping_spiders/spiders/basic_baskets.py
@@ -12,20 +12,9 @@ class BasicBasketsSpider(Spider):
 
     custom_settings = {
         'FILES_STORE': '/tmp/',
-        'FILES_STORE_S3': 's3://jumping-spiders/jumping_spiders/basic_baskets/',
         'ITEM_PIPELINES': {
-            'jumping_spiders.pipelines.BasicBasketsDuplicatesFilterPipeline': 100,
             'jumping_spiders.pipelines.BasicBasketsPdfsDownloadPipeline': 200,
             'jumping_spiders.pipelines.BasicBasketsPdfsToCsvsPipeline': 300,
-            'jumping_spiders.pipelines.BasicBasketsFilesUploadPipeline': 400,
-            'jumping_spiders.pipelines.BasicBasketsPdfsIndexingPipeline': 500,
-        },
-        'DATABASE_SETTINGS': {
-            'host': 'jumping-spiders.czko62ocualm.us-east-2.rds.amazonaws.com',
-            'port': 5432,
-            'username': 'postgres',
-            'password': '50F2AUiw5vvIMwkZX90a',
-            'database': 'jumping_spiders',
         },
     }
 
