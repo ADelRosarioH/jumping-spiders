237eaadeace9457457343bceeb31f7a314195e5d
Anthony <adelrosarioh@gmail.com>
2020-07-09 15:59:44 -0400

    fix: validates dates when missing part

--- jumping-spiders/jumping_spiders/utils/dates.py
+++ jumping-spiders/jumping_spiders/utils/dates.py
@@ -27,23 +27,25 @@ def get_from_date(text):
     days = get_days(text)
     years = get_years(text)
 
-    month = _get_parsed_month(months[0])
-    day = days[0]
-    year = years[0]
+    month = _get_parsed_month(months[0] if len(months) >= 1 else 1)
+    day = days[0] if len(days) >= 1 else 1
+    year = years[0] if len(years) >= 1 else 2017
 
     date = datetime.datetime(year=int(year), month=int(month), day=int(day))
     return date
 
 
 def get_to_date(text):
+    from_date = get_from_date(text)
+
     months = get_months(text)
     days = get_days(text)
     years = get_years(text)
 
     month = _get_parsed_month(months[1]) if len(
-        months) > 1 else _get_parsed_month(months[0])
-    day = days[1] if len(days) > 1 else days[0]
-    year = years[1] if len(years) > 1 else years[0]
+        months) > 1 else from_date.month
+    day = days[1] if len(days) > 1 else from_date.day
+    year = years[1] if len(years) > 1 else from_date.year
 
     date = datetime.datetime(year=int(year), month=int(month), day=int(day))
     return date
